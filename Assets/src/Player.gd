bf3c133539edff0f15d16f5990d970ddba874e5c
Akhilan Ganesh <akhilan.ganesh@gmail.com>
2020-09-20 00:07:53 -0700

    Added complete wisp functionality

--- Assets/src/Player.gd
+++ Assets/src/Player.gd
@@ -1,14 +1,10 @@
 extends Actor
 
-enum ATTACK_STATE {NEUTRAL, DASHING, DASHED}
-
 var vertical_input_strength
 var horizontal_input_strength
 var current_vertical_speed = Vector2(0, 0)
 var current_horizontal_speed = Vector2(0, 0)
 var direction
-var mouse_direction
-var attack_state = ATTACK_STATE.NEUTRAL
 
 export var speed:= 500
 export var drag_weight:= 10
@@ -21,35 +17,11 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+#func _process(delta):
+#	pass
 
 func _physics_process(delta):
-	# if(attack_state == ATTACK_STATE.DASHING):
-	# 	pass
-	# else:
-		movement(delta)
-	# print( get_viewport().get_mouse_position() )
-
-func movement(delta):
 	vertical_input_strength = Input.get_action_strength("Down") - Input.get_action_strength("Up")
 	horizontal_input_strength = Input.get_action_strength("Right") - Input.get_action_strength("Left")
 	direction = Vector2(horizontal_input_strength, vertical_input_strength).normalized()
 	velocity = velocity.linear_interpolate( direction*speed, delta*drag_weight )
-
-func dash():
-	yield(get_tree().create_timer(0.5), "timeout")
-	print("Hello")
-	print(get_angle_to(get_global_mouse_position()))
-	var radians = get_angle_to(get_global_mouse_position())
-	position += Vector2(cos(radians), sin(radians)) * 100
-	attack_state = ATTACK_STATE.NEUTRAL
-	# position += get_angle_to(get_global_mouse_position())
-	
-
-func _on_DashCast_dashPressed(objectHit):
-	print("hi")
-	if (objectHit != null and attack_state != ATTACK_STATE.DASHING):
-		print(attack_state)
-		attack_state = ATTACK_STATE.DASHING
-		dash()
--- Assets/src/Wisp.gd
+++ Assets/src/Wisp.gd
@@ -1,22 +1,30 @@
 extends Actor
 
-export (NodePath) var magnetCol
-export (NodePath) var realCol
+# movement speed when directing to player
+export var speed := 500
 
-onready var magnetColID:= get_node(magnetCol).get_instance_id()
-onready var realColID:  = get_node(realCol).get_instance_id()
-var collisionID:int = 0
-var inMagRange: = false
+# current player in range
+var player:Actor = null
 
-
-func _ready() -> void:
-	velocity = 50
-
-func _physics_process(delta: float) -> void:
-	collisionID = move_and_collide(velocity).get_collider_id()
-	if (collisionID == realColID):
-		queue_free()
-	elif (collisionID == magnetColID):
-			inMagRange = true
+# If player in range, moves toward player; if not, slows to stop
+func _physics_process(_delta: float) -> void:
+	if player:
+		velocity = position.direction_to(player.position).normalized() * speed
 	else:
-		inMagRange = false
+		if (velocity.length() < speed):
+			velocity = velocity - velocity.normalized() * speed
+		else:
+			velocity = Vector2.ZERO
+	move_and_slide(velocity)
+
+# If a player enters magnet range, is detected
+func _on_Magnet_body_entered(body: Node) -> void:
+	player = body as Actor
+
+# If a player leaves magnet range, no longer is detected
+func _on_Magnet_body_exited(body: Node) -> void:
+	player = null
+
+# If a player comes into contact with wisp, wisp will disappear
+func _on_PlayerTouch_body_entered(body: Node) -> void:
+	queue_free()
