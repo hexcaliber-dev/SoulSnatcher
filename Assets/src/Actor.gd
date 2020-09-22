97108a5e21a6305ade28277a03296aaebee5c510
Epicrider <omarepic1@gmail.com>
2020-09-22 14:31:19 -0700

    Add Random Tile Generation

---
+++ Assets/src/Actor.gd
@@ -0,0 +1,18 @@
+extends KinematicBody2D
+class_name Actor
+
+var velocity = Vector2.ZERO
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Engine.set_target_fps(Engine.get_iterations_per_second())
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _physics_process(delta):
+	move_and_slide(velocity)
---
+++ Assets/src/VoidMap.gd
@@ -0,0 +1,28 @@
+extends TileMap
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var rng = RandomNumberGenerator.new()
+
+onready var tile_ids_array = tile_set.get_tiles_ids()
+
+#func set_cell(x, y, tile, flip_x=false, flip_y=false, transpose=false, autotile_coord=Vector2()):
+#	rng.randi_range(0, 1)
+#	#set_cell(x, y, tile_ids_array[ rng.randi_range(0, 1) % tile_ids_array.size()], flip_x, flip_y, transpose, autotile_coord)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var cells = get_used_cells()
+	
+	print(cells)
+	for cell in cells:
+		set_cell(cell.x, cell.y, rng.randi_range(0, tile_ids_array.size()) )
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
