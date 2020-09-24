4e3bfee2b2a867c49867ec742e5a3bf9411fac80
Epicrider <omarepic1@gmail.com>
2020-09-23 18:54:08 -0700

    Add Line for Slash Dash

---
+++ Assets/src/DashLine.gd
@@ -0,0 +1,20 @@
+extends Line2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	look_at(get_global_mouse_position())
+	update()
+
+func _draw():
+	draw_line(Vector2(0,0), Vector2(1000, 0), Color(255, 255, 255), 1)
