0c10d4f23d49a0ddedda33a593131a8b4142ebc9
Epicrider <omarepic1@gmail.com>
2020-09-24 15:59:02 -0700

    Make Hit Range Larger for Slash Dash

--- Assets/src/Enemy.gd
+++ Assets/src/Enemy.gd
@@ -9,6 +9,7 @@ export var wisp_drop_number:= 2
 onready var PlayerObject:= get_node(playerObjectPath)
 onready var wispScene:= load("res://Assets/src/Wisp.tscn")
 
+var dead = false
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -35,12 +36,15 @@ func _physics_process(delta):
 		PlayerObject = get_parent().get_node("Player")
 
 func die():
-	# play death Animation
-	if PlayerObject != null:
-		yield(get_tree().create_timer(PlayerObject.dash_start_time), "timeout")
-	var wisp_instance = wispScene.instance()
-	get_parent().add_child(wisp_instance)
-	# wisp_instance.position = position
-	wisp_instance.set_position(position)
-	yield(get_tree().create_timer(0.2), "timeout") # Temporary until death animation
-	queue_free()
+	if (!dead):
+		dead = true
+		# play death Animation
+		
+		if PlayerObject != null:
+			yield(get_tree().create_timer(PlayerObject.dash_start_time), "timeout")
+		var wisp_instance = wispScene.instance()
+		get_parent().add_child(wisp_instance)
+		# wisp_instance.position = position
+		wisp_instance.set_position(position)
+		yield(get_tree().create_timer(0.2), "timeout") # Temporary until death animation
+		queue_free()
