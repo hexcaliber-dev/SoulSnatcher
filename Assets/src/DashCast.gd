2a8ae29f759af737e8b366f734cbfe7a7f0d0505
Epicrider <omarepic1@gmail.com>
2020-12-27 15:21:34 -0800

    Alter Dash to Space Button or Left Mouse Click on Release

--- Assets/src/DashCast.gd
+++ Assets/src/DashCast.gd
@@ -14,7 +14,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (Input.get_action_strength("Dash")):
+	if (Input.is_action_just_released("Dash")):
 		# print("hello")
 		#if( is_colliding() ): Uncomment when want only when hitting enemy
 		emit_signal("dashPressed", get_collider() )
